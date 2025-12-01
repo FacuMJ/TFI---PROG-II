c880050dac66d52c38087491d6f84ed61f3bd894
Iara Avila Lopez <iaraavilalopez@gmail.com>
2025-12-01 13:22:12 -0300

    Implemente: Modifique String en GestionMenuUsuario, agregue clientes data para listar como ejemplo.

--- src/main/java/com/gestionCafeteria/modelo/Cliente.java
+++ src/main/java/com/gestionCafeteria/modelo/Cliente.java
@@ -4,7 +4,7 @@ public class Cliente extends Persona {
     private int puntos; //Acumula puntos con sus reservas, obtiene promociones en base a los puntos
 
 
-    public Cliente(int dni, String nombre, String apellido, String email) {
+    public Cliente(int dni, String nombre, String apellido, String email, int puntos) {
         super(dni, nombre, apellido, email);
         this.puntos = 0; // Cuando se crea un cliente, empieza con 0 puntos.
     }
